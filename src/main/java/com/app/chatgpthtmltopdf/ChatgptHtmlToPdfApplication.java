f7788130bb920ac27fcd19e30456ca5a7d6f5462
sajjad-h <59442218+sajjad-h@users.noreply.github.com>
2023-03-13 18:17:57 +0600

    initial spring web project setup

---
+++ src/main/java/com/app/chatgpthtmltopdf/ChatgptHtmlToPdfApplication.java
@@ -0,0 +1,13 @@
+package com.app.chatgpthtmltopdf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatgptHtmlToPdfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatgptHtmlToPdfApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/chatgpthtmltopdf/ChatgptHtmlToPdfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.chatgpthtmltopdf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatgptHtmlToPdfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
